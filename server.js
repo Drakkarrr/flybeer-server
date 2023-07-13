0eaa81024d1f7d9c2a4230e1f915f8a026ce5eba
Drakkarrr <junrey404@gmail.com>
2023-07-13 16:49:18 +0800

    setup server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server successfully running on port 3000'));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
